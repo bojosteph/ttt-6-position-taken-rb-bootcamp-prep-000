3a6ef22f5b5d7d195e32f1cd3e91c4f183adc084
Roberto Samia <bojosteph@gmail.com>
2018-11-12 15:02:31 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,10 @@
 def position_taken?(board, index)
    if board[index] == nil
       false
-   elsif board[index] == "" 
+   elsif board[index] == '' || " "
       false
-    elsif board[index] == " "
-      false
+
+    
     else board[index] == "X" || "O"
       true  
    end
